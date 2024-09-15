fecfb3cab04ece6132b6cb5d119c1514056c5dc5
katzhukk <katzhukova230@gmail.com>
2024-09-15 17:25:33 +0300

    add SelenideСheckTest

---
+++ src/test/java/SelenideСheckTest.java
@@ -0,0 +1,35 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideСheckTest {
+
+    @BeforeAll
+    static void beforeAll() {/* Всегда пишется со static. Вызывается один перед всеми тестами в этом тестовом классе*/
+        Configuration.browserSize = "1920x1080";        /* Задаем разрешение браузера */
+        Configuration.pageLoadStrategy = "eager";       /* Не ждем, когда загрузится полностью страница, чтобы долго не ждать*/
+        Configuration.holdBrowserOpen = true;         /* Не дает закрыть тесту браузер. Нужно только для отладки */
+    }
+
+    @AfterAll
+    static void afterAll() {/* Всегда пишется со static. Вызывается один после всеми тестов в этом тестовом классе*/
+        WebDriverRunner.closeWebDriver();
+    }
+
+    @Test
+    void fillFormTest() {
+        //На главной странице GitHub выберите: Меню -> Solutions -> Enterprize (с помощью команды hover для Solutions).
+        open("https://github.com/"); /*Открывает форму automation-practice-form*/
+        $(".HeaderMenu-nav").$(byText("Solutions")).hover();
+        $("[href='https://github.com/enterprise']").click();
+
+        //Убедиться, что загрузилась нужная страница (например, что заголовок: "The AI-powered developer platform.").
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered\ndeveloper platform."));
+    }
+}
