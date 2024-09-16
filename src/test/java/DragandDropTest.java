b9255cf29392ef327243007b396da07da0a4f6aa
katzhukk <katzhukova230@gmail.com>
2024-09-16 21:11:09 +0300

    add DragandDropTest.java

--- src/test/java/DragandDropTest.java
+++ src/test/java/DragandDropTest.java
@@ -17,7 +17,7 @@ public class DragandDropTest {
     static void beforeAll() {/* Всегда пишется со static. Вызывается один перед всеми тестами в этом тестовом классе*/
         Configuration.browserSize = "1920x1080";        /* Задаем разрешение браузера */
         Configuration.pageLoadStrategy = "eager";       /* Не ждем, когда загрузится полностью страница, чтобы долго не ждать*/
-        Configuration.holdBrowserOpen = true;         /* Не дает закрыть тесту браузер. Нужно только для отладки */
+        //Configuration.holdBrowserOpen = true;         /* Не дает закрыть тесту браузер. Нужно только для отладки */
     }
 
     @AfterAll
@@ -30,11 +30,11 @@ public class DragandDropTest {
         //(опциональное) Запрограммируйте Drag&Drop с помощью Selenide.actions()
         //Откройте https://the-internet.herokuapp.com/drag_and_drop
         open("https://the-internet.herokuapp.com/drag_and_drop");
+
         //Перенесите прямоугольник А на место В:
         // * Поднести курсор к прямоугольнику А   // moveToElement($("#column-a"))
         // * Кликнуть на прямоугольник А и зажать // clickAndHold()
         // * Перенести прямоугольник А на место В // moveByOffset(x, y)
-
         actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(200, 0).release().perform(); //перенос прямоугольника А на 200 px по оси X
 
         //Проверьте, что прямоугольники действительно поменялись
