707551897c59cb1ef537201a5617ccffe5f4976f
katzhukk <katzhukova230@gmail.com>
2024-09-17 22:46:25 +0300

    add DragandDropTest.java

--- src/test/java/DragandDropTest.java
+++ src/test/java/DragandDropTest.java
@@ -1,8 +1,7 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selenide.*;
@@ -13,6 +12,7 @@ public class DragandDropTest {
     static void beforeAll() {/* Всегда пишется со static. Вызывается один перед всеми тестами в этом тестовом классе*/
         Configuration.browserSize = "1920x1080";        /* Задаем разрешение браузера */
         Configuration.pageLoadStrategy = "eager";       /* Не ждем, когда загрузится полностью страница, чтобы долго не ждать*/
+        Configuration.baseUrl = "https://the-internet.herokuapp.com/";
         //Configuration.holdBrowserOpen = true;         /* Не дает закрыть тесту браузер. Нужно только для отладки */
     }
 
@@ -22,27 +22,37 @@ public class DragandDropTest {
     }
 
     @Test
-    void moveToElementTest() throws InterruptedException {
-        //(опциональное) Запрограммируйте Drag&Drop с помощью Selenide.actions()
-        //Откройте https://the-internet.herokuapp.com/drag_and_drop
-        open("https://the-internet.herokuapp.com/drag_and_drop");
+    void moveToElementTest(){
+        //Откроем https://the-internet.herokuapp.com/drag_and_drop
+        open("/drag_and_drop");
 
-        //Перенесите прямоугольник А на место В:
-        // * Поднести курсор к прямоугольнику А   // moveToElement($("#column-a"))
-        // * Кликнуть на прямоугольник А и зажать // clickAndHold()
-        // * Перенести прямоугольник А на место В // moveByOffset(x, y)
+        //Проверим, что прямоугольник А и прямоугольник B стоят на своих местах
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+
+        //Перенесем прямоугольник А на место В:
         actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(200, 0).release().perform(); //перенос прямоугольника А на 200 px по оси X
 
-        //Проверьте, что прямоугольники действительно поменялись
+        //Проверим, что прямоугольники действительно поменялись
         $("#column-a").shouldHave(text("B"));
 
-        //подождем 2 секунды
-        Thread.sleep(2000);
+        WebDriverRunner.closeWindow();
+    }
+
+    @Test
+    void DropTest(){
+        open("/drag_and_drop");
+
+        //Проверим, что прямоугольник А и прямоугольник B стоят на своих местах
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
 
         //В Selenide есть команда $(element).dragAndDrop($(to-element)), проверьте работает ли тест, если использовать её вместо actions()
         $("#column-b").dragAndDrop(to("#column-a"));
 
-        //Проверьте, что прямоугольники действительно поменялись
-        $("#column-a").shouldHave(text("A"));
+        //Проверим, что прямоугольники действительно поменялись
+        $("#column-a").shouldHave(text("B"));
+
+        WebDriverRunner.closeWindow();
     }
 }
